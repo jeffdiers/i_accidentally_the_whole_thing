6bead34226849345070d5506c6aaaf3580b345c4
Patrick Rauls <patrickrauls@Patricks-MacBook-Air.local>
2017-02-10 12:19:26 -0700

    Initial Commit

--- components.js
+++ components.js
@@ -1,40 +1,52 @@
 (function() {
-  angular.module('app', [])
-  .component('giphy', {
-    controller: giphy,
-    templateUrl: './templates/giphy.html'
-  })
-  .component('signup', {
-    controller: signup,
-    templateUrl: './templates/signup.html'
-  })
-  .component('ad', {
-    controller: ad,
-    templateUrl: './templates/ad.html'
-  })
-  .component('products', {
-    controller: products,
-    templateUrl: './templates/products.html'
-  })
-  .component('adele', {
-    controller: adele,
-    templateUrl: './templates/adele.html'
-  })
+    angular.module('app', [])
+        .component('giphy', {
+            controller: giphy,
+            templateUrl: './templates/giphy.html'
+        })
+        .component('signup', {
+            controller: signup,
+            templateUrl: './templates/signup.html'
+        })
+        .component('ad', {
+            controller: ad,
+            templateUrl: './templates/ad.html'
+        })
+        .component('products', {
+            controller: products,
+            templateUrl: './templates/products.html'
+        })
+        .component('adele', {
+            controller: adele,
+            templateUrl: './templates/adele.html'
+        })
 
-  function ad(){
-console.log('hi ad')
-  }
-  function giphy(){
-console.log('hi giphy')
-  }
-  function adele(){
-console.log('hi adele')
-  }
-  function products(){
-console.log('hi products')
-  }
-  function signup(){
-console.log('hi signup')
-  }
+    function ad() {
+        const vm = this;
+        console.log('ad', vm);
+    }
+
+    function giphy() {
+        const vm = this;
+        console.log('giphy', vm);
+    }
+
+    function adele() {
+        const vm = this;
+        console.log('adele', vm);
+    }
+
+    function products() {
+        const vm = this;
+        console.log('products', vm);
+    }
+
+    function signup() {
+        const vm = this;
+        vm.postContact = () => {
+          vm.thanks = true;
+        }
+        console.log('signup', vm);
+    }
 
 })();
